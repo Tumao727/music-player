45de9abb8aa4e508f493c0d3459071337af573e0
tumao <tumao@protonmail.com>
2018-09-28 17:28:11 +0800

    completed showing total time and current time function

--- player.js
+++ player.js
@@ -9,8 +9,40 @@ const audioPlayOrPause = (audio) => {
     })
 }
 
+const time = (timeLength) => {
+    let min = Math.floor(timeLength / 60)
+    let sec = Math.floor(timeLength % 60)
+    // 调整时间展示格式
+    if (sec < 10) {
+        sec = '0' + String(sec)
+    }
+    let result = `${min}:${sec}`
+    return result
+}
+
+const audioTime = (audio) => {
+    let totalTime = audio.duration
+    let currentTime = audio.currentTime
+
+    let current = e('#id-span-current')
+    let separator = e('#id-span-separator')
+    let total = e('#id-span-total')
+
+    current.innerHTML = time(currentTime)
+    separator.innerHTML = ' / '
+    total.innerHTML = time(totalTime)
+}
+
+const showAudioTime = (audio) => {
+    let interval = 300
+    setInterval(() => {
+        audioTime(audio)
+    }, interval)
+}
+
 const bindEvents = (a) => {
     audioPlayOrPause(a)
+    showAudioTime(a)
 }
 
 const __main = () => {
