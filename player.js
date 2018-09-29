2f8c3cc7ca914b7c34dbe3e70b642cb86eb2b744
tumao <tumao@protonmail.com>
2018-09-29 10:33:50 +0800

    Add album and complete shifting picture by clicking next or last button

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+// 播放及暂停
 const audioPlayOrPause = (audio) => {
     let button = e('#id-span-play')
     bindEvent(button, 'click', () => {
@@ -9,6 +10,7 @@ const audioPlayOrPause = (audio) => {
     })
 }
 
+// 显示当前播放时长与歌曲总长度
 const time = (timeLength) => {
     let min = Math.floor(timeLength / 60)
     let sec = Math.floor(timeLength % 60)
@@ -43,50 +45,144 @@ const showAudioTime = (audio) => {
     }, interval)
 }
 
+// 上一首 与 下一首 点击切换
 const nextSong = (() => {
     let songs = [
-        'music/1.mp3',
-        'music/2.mp3',
-        'music/3.mp3',
+        {
+            song: 'music/1.mp3',
+            pic: 'picture/1.jpg',
+            name: 'Loser',
+            singer: '米津玄師',
+        },
+        {
+            song: 'music/2.mp3',
+            pic: 'picture/2.jpg',
+            name: 'DROP',
+            singer: `ROOKiEZ is PUNK'D`,
+        },
+        {
+            song: 'music/3.mp3',
+            pic: 'picture/3.jpg',
+            name: 'Terror',
+            singer: 'まふまふ',
+        },
     ]
     let index = 0
     return () => {
         index = (index + 1) % songs.length
-        return songs[index]
+        return songs[index]['song']
     }
 })()
 
 const lastSong = (() => {
     let songs = [
-        'music/1.mp3',
-        'music/2.mp3',
-        'music/3.mp3',
+        {
+            song: 'music/1.mp3',
+            pic: 'picture/1.jpg',
+            name: 'Loser',
+            singer: '米津玄師',
+        },
+        {
+            song: 'music/2.mp3',
+            pic: 'picture/2.jpg',
+            name: 'DROP',
+            singer: `ROOKiEZ is PUNK'D`,
+        },
+        {
+            song: 'music/3.mp3',
+            pic: 'picture/3.jpg',
+            name: 'Terror',
+            singer: 'まふまふ',
+        },
     ]
     let index = 0
     return () => {
         index = (index + songs.length - 1) % songs.length
-        return songs[index]
+        return songs[index]['song']
     }
 })()
 
-const playLast = (audio) => {
+// 图片 上一张 与 下一张 切换
+const nextPic = (() => {
+    let songs = [
+        {
+            song: 'music/1.mp3',
+            pic: 'picture/1.jpg',
+            name: 'Loser',
+            singer: '米津玄師',
+        },
+        {
+            song: 'music/2.mp3',
+            pic: 'picture/2.jpg',
+            name: 'DROP',
+            singer: `ROOKiEZ is PUNK'D`,
+        },
+        {
+            song: 'music/3.mp3',
+            pic: 'picture/3.jpg',
+            name: 'Terror',
+            singer: 'まふまふ',
+        },
+    ]
+    let index = 0
+    return () => {
+        index = (index + 1) % songs.length
+        return songs[index]['pic']
+    }
+})()
+
+const lastPic = (() => {
+    let songs = [
+        {
+            song: 'music/1.mp3',
+            pic: 'picture/1.jpg',
+            name: 'Loser',
+            singer: '米津玄師',
+        },
+        {
+            song: 'music/2.mp3',
+            pic: 'picture/2.jpg',
+            name: 'DROP',
+            singer: `ROOKiEZ is PUNK'D`,
+        },
+        {
+            song: 'music/3.mp3',
+            pic: 'picture/3.jpg',
+            name: 'Terror',
+            singer: 'まふまふ',
+        },
+    ]
+    let index = 0
+    return () => {
+        index = (index + songs.length - 1) % songs.length
+        return songs[index]['pic']
+    }
+})()
+
+
+const playLast = (audio, img) => {
     let last = e('#id-span-last')
     bindEvent(last, 'click', () => {
         let song = lastSong()
         audio.src = song
         audio.play()
+        let pic = lastPic()
+        img.src = pic
     })
 }
 
-const playNext = (audio) => {
+const playNext = (audio, img) => {
     let next = e('#id-span-next')
     bindEvent(next, 'click', () => {
         let song = nextSong()
         audio.src = song
         audio.play()
+        let pic = nextPic()
+        img.src = pic
     })
 }
 
+// 随机播放
 const randomNumber = (range) => {
     let number = Math.random() * range
     return Math.floor(number)
@@ -117,6 +213,15 @@ const random = (audio) => {
     })
 }
 
+const randomPlay = (audio) => {
+    bindEvent(audio, 'ended', () => {
+        let song = randomSong()
+        audio.src = song
+        audio.play()
+    })
+}
+
+// 循环播放
 const loop = (audio) => {
     let button = e('#id-span-loop')
     bindEvent(button, 'click', () => {
@@ -134,6 +239,13 @@ const loop = (audio) => {
     })
 }
 
+const loopPlay = (audio) => {
+    bindEvent(audio, 'ended', () => {
+        audio.play()
+    })
+}
+
+// 顺序播放
 const orderPlay = (audio) => {
     bindEvent(audio, 'ended', () => {
         let song = nextSong()
@@ -142,24 +254,9 @@ const orderPlay = (audio) => {
     })
 }
 
-const loopPlay = (audio) => {
-    bindEvent(audio, 'ended', () => {
-        audio.play()
-    })
-}
-
-const randomPlay = (audio) => {
-    bindEvent(audio, 'ended', () => {
-        let song = randomSong()
-        log(song)
-        audio.src = song
-        audio.play()
-    })
-}
-
+// 确定播放模式
 const bindEventEnded = (audio) => {
     let s = audio.dataset.status
-    log(s)
     if (s === 'order') {
         orderPlay(audio)
     }
@@ -176,18 +273,19 @@ const playStatus = (audio) => {
     loop(audio)
 }
 
-const bindEvents = (a) => {
+const bindEvents = (a, img) => {
     audioPlayOrPause(a)
     showAudioTime(a)
-    playLast(a)
-    playNext(a)
+    playLast(a, img)
+    playNext(a, img)
     playStatus(a)
     bindEventEnded(a)
 }
 
 const __main = () => {
     let a = e('#id-audio-player')
-    bindEvents(a)
+    let img = e('img')
+    bindEvents(a, img)
 }
 
 __main()
